df34c3ee23f093854b6d08dd4d943903eb14f826
Alen Eminovic <emal24na@student.ju.se>
2024-11-27 11:25:50 +0100

    Alen & Hedda

--- game.js
+++ game.js
@@ -1,7 +1,24 @@
+function preload() {
+  jthBoyBack = loadImage("/assets/jthBoyBack.png");
+}
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(600, 800);
+  character = new Character(0, 0);
+}
+
+class Character {
+  constructor(x, y) {
+    this.characterX = x;
+    this.characterY = y;
+  }
+
+  draw() {
+    image(jthBoyBack, this.characterX, this.characterY);
+  }
+  characterMove() {}
 }
 
 function draw() {
-  background(255, 140, 0);
+  character.draw();
 }
