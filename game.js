f9efa60edfab8031ff0c32b6dd251b2a38e69cf9
pehe24kv <pehe24kv@student.ju.se>
2024-11-28 13:33:44 +0100

    Made a grid
    A grid was made.
    Hedda and Alen

--- game.js
+++ game.js
@@ -5,56 +5,81 @@
     NMD24
 */
 
-class Enemy {
-  constructor(x, y) {
-    this.enemyX = 10;
-    this.enemyY = 10;
-    this.width = 45;
-    this.height = 80;
-  }
-
-  draw() {
-    fill(255);
-    rect(this.enemyX, this.enemyY, this.width, this.height);
-  }
-}
-
-function gameScreen() {
-  enemies.draw();
-}
-
-function draw() {
-  background(223, 229, 255);
-  gameScreen();
-}
-
-let x = 85;
-let y = 50;
-let enemies = [];
-
 function setup() {
   createCanvas(600, 800);
 }
 
-class Enemy {
-  constructor(x, y) {
-    this.enemyX = 10;
-    this.enemyY = 10;
-    this.width = 45;
-    this.height = 80;
-  }
+//Grid system
+const gridLength = 12;
+const gridSize = 60;
+let counter = 0;
 
-  draw() {
-    fill(255);
-    rect(this.enemyX, this.enemyY, this.width, this.height);
+function drawGrid() {
+  push();
+  stroke(255, 255, 255);
+  fill(0);
+  for (let x = 0; x < gridLength; x++) {
+    for (let y = 0; y < gridLength; y++) {
+      rect(x * gridSize, y * gridSize, gridSize, gridSize);
+    }
   }
-}
-
-function gameScreen() {
-  enemies.draw();
+  pop();
 }
 
 function draw() {
-  background(223, 229, 255);
-  gameScreen();
+  drawGrid();
 }
+
+// class Enemy {
+//   constructor(x, y) {
+//     this.enemyX = 10;
+//     this.enemyY = 10;
+//     this.width = 45;
+//     this.height = 80;
+//   }
+
+//   draw() {
+//     fill(255);
+//     rect(this.enemyX, this.enemyY, this.width, this.height);
+//   }
+// }
+
+// function gameScreen() {
+//   enemies.draw();
+// }
+
+// function draw() {
+//   background(223, 229, 255);
+//   gameScreen();
+// }
+
+// let x = 85;
+// let y = 50;
+// let enemies = [];
+
+// function setup() {
+//   createCanvas(600, 800);
+// }
+
+// class Enemy {
+//   constructor(x, y) {
+//     this.enemyX = 10;
+//     this.enemyY = 10;
+//     this.width = 45;
+//     this.height = 80;
+//   }
+
+//   draw() {
+//     fill(255);
+//     rect(this.enemyX, this.enemyY, this.width, this.height);
+//   }
+// }
+
+// function gameScreen() {
+//   enemies.draw();
+// }
+
+// function draw() {
+//   background(223, 229, 255);
+//   gameScreen();
+// }
