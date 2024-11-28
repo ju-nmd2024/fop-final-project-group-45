2acba8f812e93bf179ac812fab6fa4d0e8578ae3
pehe24kv <pehe24kv@student.ju.se>
2024-11-28 15:14:16 +0100

    Made enemies
    Made enemies for the game, and made them move
    Hedd and Alen

--- enemies.js
+++ enemies.js
@@ -1,22 +1,42 @@
+function setup() {
+  createCanvas(600, 800);
+}
+
 class Enemy {
   constructor(x, y, width, height) {
     this.enemyX = x;
     this.enemyY = y;
-    this.enemyWidth = width;
-    this.enemyHeight = height;
+    this.size = 40;
   }
 
   draw() {
     push();
-    fill(255);
-    rect(this.enemyX, this.enemyY, this.enemyWidth, this.enemyHeight);
+    fill(0);
+    ellipse(this.enemyX, this.enemyY, this.size);
     pop();
+  }
+
+  move(speed) {
+    this.enemyX += speed;
   }
 }
 
-const oneEnemy = new Enemy(250, 600, 45, 80);
+let enemies = [];
+let rows = 5;
+let columns = 8;
+
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < columns; j++) {
+    let x = 50 + j * 50;
+    let y = 50 + i * 50;
+    enemies.push(new Enemy(x, y));
+  }
+}
 
 function draw() {
-  clear();
-  oneEnemy.draw();
+  background(255);
+  for (let Enemy of enemies) {
+    Enemy.draw();
+    Enemy.move(1);
+  }
 }
