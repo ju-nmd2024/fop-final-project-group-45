e360ba1bae58503a8ca8bbf4658d788d1e3eb7d4
pehe24kv <pehe24kv@student.ju.se>
2024-11-28 20:00:23 +0100

    The enemies are turning around
    Hedd and Alen

--- enemies.js
+++ enemies.js
@@ -14,15 +14,21 @@ class Enemy {
   draw() {
     push();
     fill(0);
-    rect(this.enemyX, this.enemyY, this.height, this.width);
+    rect(this.enemyX, this.enemyY, this.width, this.height);
     pop();
   }
 
   move() {
     this.enemyX += this.speed;
   }
+
+  //help from https://chatgpt.com/share/6748b4c9-1d70-8000-94ca-828771c018ba
+  moveDown() {
+    this.enemyY += 40;
+  }
+  //help stopped
 }
-const enemy = new Enemy(10, 10, 45, 80);
+const enemy = new Enemy(10, 10, 0, 0);
 
 let enemies = [];
 let rows = 5;
@@ -32,14 +38,38 @@ for (let i = 0; i < rows; i++) {
   for (let j = 0; j < columns; j++) {
     let x = 50 + j * 50;
     let y = 50 + i * 50;
-    enemies.push(new Enemy(x, y, 45, 30));
+    enemies.push(new Enemy(x, y, 30, 40));
   }
 }
 
 function draw() {
   background(255);
+  //help from https://chatgpt.com/share/6748b4c9-1d70-8000-94ca-828771c018ba
+  let edgeReached = false;
+
   for (let Enemy of enemies) {
     Enemy.draw();
     Enemy.move();
+
+    //maked that enemies have reached an edge
+    if (Enemy.enemyX <= 0 || Enemy.enemyX + Enemy.width >= 600) {
+      edgeReached = true;
+    }
   }
-}
+
+  if (edgeReached) {
+    for (let enemy of enemies) {
+      enemy.speed *= -0.5;
+      enemy.moveDown();
+    }
+  }
+} // help done
+
+// //trying to make the array reversed
+// function reverse(enemies) {
+//   let reversedArray = [];
+//   for (let element of array) {
+//     reversedArray.unshift(element);
+//   }
+//   return reversedArray;
+//}
